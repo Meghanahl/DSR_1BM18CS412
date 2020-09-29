22cfae9304b2054f7e9e1b27db16628203aa9abe
Meghanahl <53852111+Meghanahl@users.noreply.github.com>
2020-09-29 12:38:33 +0530

    Add files via upload

---
+++ program2/lab_prog2.R
@@ -0,0 +1,12 @@
+BuffTail<-c(10,20,30,40,50)
+Gardenbee<-c(1,2,3,4,5)
+RedTail<-c(5,10,15,20,25)
+Carderbee<-c(5,6,3,8,2)
+HoneyBee<-c(12,34,12,44,56)
+beeframe<-data.frame(BuffTail,Gardenbee,RedTail,Carderbee,HoneyBee)
+beeframe
+names<-c("Thistle","Vipers","GoldenRain","Yellowalfala","blackberry")
+rownames(beeframe)<-names
+beeframe
+
+
