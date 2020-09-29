26be4ea99e216d51a4e274f61a47b43ec70518ba
Meghanahl <53852111+Meghanahl@users.noreply.github.com>
2020-09-29 00:11:10 -0700

    Delete prog1.R

--- lab1/prog1.R
+++
@@ -1,7 +0,0 @@
-# write a r code to create list of data objects of type numeric,string,real numbers and
-
-N1<-c(10,20,30,40)
-str1<-c("meghana","disha","priyanka")
-Rlnum<-c(23.2,33.3,44.4,55.5)
-List1<-list(N1,str1,Rlnum)
-List1
