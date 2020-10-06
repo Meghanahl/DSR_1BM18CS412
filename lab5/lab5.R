a0e295ed1d0828747014db8d68053645c2bf66b1
Meghanahl <53852111+Meghanahl@users.noreply.github.com>
2020-10-06 01:58:30 -0700

    Delete lab5.R

--- lab5/lab5.R
+++
@@ -1,9 +0,0 @@
-#Q5. Export a vector to 3 txt files.
-data <- c(23.0,17.0,12.5,11.0,17.0,12.0,14.5,9.0,11.0,9.0,12.5,14.5,17.0)
-data
-length(data)
-write(data, file = "C:/Users/Meghana H L/Documents/lab5/data1.txt",sep = "\t")
-# creating a columnwise output based on length
-write(data, file = "C:/Users/Meghana H L/Documents/lab5/data2.txt",sep=",",ncolumns = length(data))
-# creating a single column
-write(data, file = "C:/Users/Meghana H L/Documents/lab5/data3.txt", sep = ",", ncolumns = 1)
